a14248d393d66f46a3fb7c81faca4c5011eec291
tonyzhangcommit <zfy18434368518@163.com>
2021-01-06 13:40:35 +0800

    测试数据库01

--- djangoDemo/Login/views.py
+++ djangoDemo/Login/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 import time
 # Create your views here.
+from django.http import JsonResponse
 
 
 def createUser(request):
-    pass
+    print('测试新的请求函数')
+    return JsonResponse({'msg':'success'})
